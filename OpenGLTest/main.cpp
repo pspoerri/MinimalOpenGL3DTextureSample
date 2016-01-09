983bcacb783048bd178a733de4b381645e099f8b
Pascal Spörri <pascal.spoerri@env.ethz.ch>
2016-01-09 17:15:32 +0100

    Initial Commit

---
+++ OpenGLTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGLTest
+//
+//  Created by Pascal Spörri on 09/01/16.
+//  Copyright © 2016 Pascal Spörri. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
